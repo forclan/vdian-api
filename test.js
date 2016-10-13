88c05931c6ce4a38534846e7ebfbe5b4430a5821
forclan <hxplian1992@163.com>
2016-10-14 00:24:18 +0800

    add test case in test.js

--- test.js
+++ test.js
@@ -5,4 +5,5 @@ var getAllItemInfo = require('./https.js').getAllItemInfo;
 // getListsInShop(1686060).then(console.log);
 // getItemsInList(1686060, 71527374, 19).then(console.log);
 // getItemDetail(1848302313).then(console.log).catch(console.log)
-getAllItemInfo(1686060).then(console.log).catch(console.log);
+// getAllItemInfo(1686060).then(console.log).catch(console.log);
+getAllItemInfo(1686060).then(val => console.log(val[0]));
