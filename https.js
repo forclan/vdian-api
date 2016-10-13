22df243856bdda04c71de7161dbc4067392e14c1
forclan <hxplian1992@163.com>
2016-10-13 20:16:17 +0800

    对部分函数添加注释

--- https.js
+++ https.js
@@ -2,8 +2,12 @@ const https = require('https');
 var request = require('./https');
 
 
-// const values
+/**
+ * const values
+ */
+// 用于获取items地址，在函数generateVdianItemInListURL中有使用
 const VDIAN_ITEM_URL = 'https://wd.api.weidian.com/wd/cate/getItemsForBuyer';
+// 用于获取店铺所有分类号的地址，在generateVdianListURL中使用
 const VDIAN_LIST_URL = 'https://wd.api.weidian.com/wd/cate/getList';
 
 // 用于获取List分类下所有的商品信息
@@ -19,18 +23,41 @@ function generateCommodityURL(URL, userID, typeNum, start = 0, needNum = 10) {
   return URL + '?param=' + JSON.stringify(param);
 }
 
+/**
+ * 返回一个用于获取List中包含Item信息的URL
+ * @param  {int} userID        店家的ID
+ * @param  {int} listNum       店家的分类号（List号）
+ * @param  {int} itemNumInList 该分类下的item数目，默认值为10
+ * @return {string}               一个URL地址，返回值为分类下的item地址
+ */
 const generateVdianItemInListURL = (userID, listNum, itemNumInList) => generateCommodityURL(VDIAN_ITEM_URL, userID, listNum, 0, itemNumInList);
-// const generateVdianCommodityURL = (userID, typeNum) => generateCommodityURL(VDIAN_ITEM_URL, userID, typeNum);
+
+/**
+ * 生成一个用于获取店铺下所有分类的URL地址
+ * @param  {int} userID 店铺的ID（店家的ID）
+ * @return {string}        用于获取所有分类号的URL地址
+ */
 const generateVdianListURL = userID => generateCommodityURL(VDIAN_LIST_URL, userID);
+
+/**
+ * 生成一个用于获取item详细信息的URL地址
+ * @param  {int} itemID 想要获取的item号
+ * @return {string}        item的URL地址
+ */
 const generateVdianItemURL = itemID => 'https://weidian.com/item.html?itemID=' + itemID + '&p=-1';
 
+/**
+ * test URLs
+ */
 
-const testString = 'https://wd.api.weidian.com/wd/cate/getItemsForBuyer?param={"userID":"1686060","cate_id":"72843023","limitStart":0,"limitNum":70}';
-// const generatedString = generateCommodityURL('wd.api.weidian.com/wd/cate/getItemsForBuyer', 1686060, 72843023);
-// const vdianItemsURL = generateVdianCommodityURL(1686060, 72843023);
-// const listURL = generateVdianListURL(1686060);
-const itemURL = generateVdianItemURL(1848302313);
+const itemsInListTestString = 'https://wd.api.weidian.com/wd/cate/getItemsForBuyer?param={"userID":"1686060","cate_id":"72843023","limitStart":0,"limitNum":70}';
+const itemDetailTestURL = generateVdianItemURL(1848302313);
 
+/**
+ * 用于发起https请求，并将结果保持在Promise中
+ * @param  {string} requestOptions 请求的URL地址
+ * @return {Promise}                包含有请求结果的Promise对象
+ */
 function httpsRequest(requestOptions) {
   var promise = new Promise((resolve, rejece) => {
     var results = '';
@@ -49,14 +76,13 @@ function httpsRequest(requestOptions) {
   return promise;
 }
 
-
-
 const item = 'https://weidian.com/item.html?itemID=1860126149&p=-1';
 // var re = httpsRequest(listURL);
 getListInShop(1686060).then(console.log);
-// re.then(getListInfoFromString).then(console.log)
 
 getItemsInList(1686060, 71527374, 19).then(console.log);
+
+
 function getListInShop(userID) {
   var requestURL = generateVdianListURL(userID);
   var promise = httpsRequest(requestURL).then(getListInfoFromString);
@@ -96,12 +122,10 @@ function getItemInfoFromHtml(htmlInString) {
   var itemSimpleInfo = simplifItemInfo(itemInfo);
   itemSimpleInfo.itemName = itemName;
   // delete unused information
-  // console.log(itemSimpleInfo);
   return itemSimpleInfo;
 }
 
-// re.then(val => getItemInfoFromHtml(val)).then(val => console.log(val));
-
+// 用于将服务器上获取的item详细信息进行简化，删除不需要的键值
 function simplifItemInfo(itemInfo) {
   const URLReg = /https:\/\/[\w\.\-\/]*/ig;
   const imgDeleteParam = URL => {
